39bad7d97247d813f9797f77cc72599c458a5ff5
lsfoo <lsfoo@qq.com>
2019-01-27 18:01:04 +0800

    g

--- customer/client/src/app/list/list.component.ts
+++ customer/client/src/app/list/list.component.ts
@@ -1,5 +1,6 @@
+import { ProductResourceService } from './../../shared/api/productResource.service'
 import { Component, OnInit } from '@angular/core'
-import { CategoryResourceService, Category } from 'src/shared';
+import { CategoryResourceService, Category, Product } from 'src/shared'
 
 @Component({
   selector: 'app-list',
@@ -8,9 +9,17 @@ import { CategoryResourceService, Category } from 'src/shared';
 })
 export class ListComponent implements OnInit {
   categories: Array<Category> = []
-  constructor(public categoryResourceService: CategoryResourceService) {
+  products: Array<Product> = []
+  constructor(
+    private categoryResourceService: CategoryResourceService,
+    private productResourceService: ProductResourceService
+  ) {
     this.categoryResourceService.getAllCategoriesUsingGET().subscribe(res => {
       this.categories = res
+    })
+    this.productResourceService.getAllProductsUsingGET().subscribe(res => {
+      console.log(res)
+      this.products = res
     })
   }
 
